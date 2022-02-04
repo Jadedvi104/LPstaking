ad79e0828d2d073b329e10eae357a04e3e4a6929
Jadedvi104 <jadedvi104@gmail.com>
2022-02-04 15:32:49 +0700

    Update LPStaking.sol

--- contracts/LPStaking.sol
+++ contracts/LPStaking.sol
@@ -49,7 +49,6 @@ contract LPStaking is Ownable {
     uint256 public totalFee;
 
     uint256 private _totalSupply;
-    uint256 private totalSupply;
 
     uint256 private MAXIMUM_STAKING = 50000000000000000000000; //50,000 LPtokens
     uint256 private TOTAL_LAKRIMA_PER_POOL = 16666666000000000000000000; //16,666,666 LKM
@@ -72,15 +71,27 @@ contract LPStaking is Ownable {
     mapping(address => uint256) private _lockedBalances;
     mapping(address => uint256) private _releaseTime;
     mapping(address => uint256) private _lockedRewards;
+    mapping(address => uint256) private rewards;
     mapping(address => uint256) private claimedReward;
     mapping(address => uint128) public stakeCounts;
-    mapping(address => mapping(uint256 => Stake)) stakers;
+    mapping(address => mapping(uint32 => Stake)) stakers; // edit
 
     // ************** Connected Address ******************** //
 
     address public lpTokenAddress;
 
     IERC20 public rewardsTokenContract;
+
+    // ************** Modifier ******************** //
+
+    modifier updateReward(address account) {
+        rewardPerTokenStored = rewardPerToken();
+        lastUpdateTime = block.timestamp;
+
+        rewards[account] = earned(account);
+        userRewardPerTokenPaid[account] = rewardPerTokenStored;
+        _;
+    }
 
     // ************** Event ******************** //
 
@@ -99,7 +110,8 @@ contract LPStaking is Ownable {
         endPool = timeEnd;
     }
 
-    // ************** Management Function ******************** //
+
+    // ************** View Functions ******************** //
 
     function checkUserLPBalance(address account) public view returns (uint256) {
         return IERC20(lpTokenAddress).balanceOf(account);
@@ -134,7 +146,7 @@ contract LPStaking is Ownable {
     }
 
     function remainingPool() public view returns (uint256) {
-        return MAXIMUM_STAKING - totalSupply;
+        return MAXIMUM_STAKING - _totalSupply;
     }
 
     function remainingReward() public view returns (uint256) {
@@ -147,6 +159,16 @@ contract LPStaking is Ownable {
         }
 
         return balances[_account];
+    }
+
+    function rewardPerToken() public view returns (uint256) {
+        if (_totalSupply == 0) {
+            return 0;
+        }
+        return
+            rewardPerTokenStored +
+            (((block.timestamp - lastUpdateTime) * rewardRate * 1e18) /
+                _totalSupply);
     }
 
     function earned(address account) public view returns (uint256) {
@@ -175,8 +197,43 @@ contract LPStaking is Ownable {
         return totalReward;
     }
 
-    /************************* SEND FUNC *****************************/
-    function unStake() external {
+    function rewards(address account) public view returns (uint256) {
+        if (_battlePowerBalances[account] == 0) {
+            return tokenRewards[account];
+        }
+
+        return
+            ((_battlePowerBalances[account] *
+                (rewardPerToken() - userRewardPerTokenPaid[account])) / 1e18) +
+            tokenRewards[account];
+    }
+
+    function canClaimReward(address account) public view returns (bool) {
+        return
+            (rewards(account) >= MINIMUM_AMOUNT_CLAIM) &&
+            (getTimestamp() >= accountLastClaim[account] + 10 days);
+    }
+
+
+    function getRank(address account) public view returns (uint256) {
+        Rank[] memory results = new Rank[](ranks.length);
+        for (uint256 index = 0; index < ranks.length; index++) {
+            results[index] = ranks[index];
+        }
+
+        results = sort(results);
+
+        for (uint256 index = 0; index < results.length; index++) {
+            if (results[index].account == account) {
+                return index + 1;
+            }
+        }
+        return 0;
+    }
+
+    /************************* ACTION FUNCTIONS *****************************/
+
+    function unStake() external updateReward(msg.sender) {
         require(balances[msg.sender] != 0);
 
         uint256 balance = balances[msg.sender];
@@ -187,7 +244,7 @@ contract LPStaking is Ownable {
 
         claimedReward = claimedReward + reward;
 
-        totalSupply = totalSupply - balance;
+        _totalSupply = _totalSupply - balance;
 
         //Clear balance
         delete stakers[msg.sender];
@@ -199,7 +256,7 @@ contract LPStaking is Ownable {
         emit UnStakeEvent(msg.sender, getTimestamp(), balance, reward);
     }
 
-    function unStakeNow() external {
+    function unStakeNow() external updateReward(msg.sender) {
         require(_lockedBalances[msg.sender] != 0);
 
         uint256 amount = _lockedBalances[msg.sender];
@@ -220,7 +277,7 @@ contract LPStaking is Ownable {
         emit UnStakeNowEvent(msg.sender, getTimestamp(), amount, reward, fee);
     }
 
-    function claim() external {
+    function claim() external updateReward(msg.sender) {
         require(_releaseTime[msg.sender] != 0);
         require(_releaseTime[msg.sender] <= getTimestamp());
         require(_lockedBalances[msg.sender] != 0);
@@ -245,10 +302,10 @@ contract LPStaking is Ownable {
         uint256 timestamp = getTimestamp();
         require(!isPoolClose(), "Pool is closed");
         require(amount <= LPBalance);
-        require(totalSupply + amount <= MAXIMUM_STAKING);
+        require(_totalSupply + amount <= MAXIMUM_STAKING);
         require(balances[msg.sender] + amount >= MINIMUM_STAKING);
 
-        totalSupply = totalSupply + amount;
+        _totalSupply = _totalSupply + amount;
         balances[msg.sender] = balances[msg.sender] + amount;
         stakers[msg.sender].push(Stake(amount, timestamp)); // need to be edited
         stakeCounts[msg.sender] = stakeCounts[msg.sender] + 1;
@@ -261,5 +318,80 @@ contract LPStaking is Ownable {
         _lockedBalances[msg.sender] = amount;
         _lockedRewards[msg.sender] = reward;
         _releaseTime[msg.sender] = getTimestamp() + 3 days; // dummy numbers
+    }
+
+    function updateRank() private {
+        if (!userHasRank[msg.sender]) {
+            userRankIds[msg.sender] = ranks.length;
+            ranks.push(
+                Rank(
+                    msg.sender,
+                    _battlePowerBalances[msg.sender],
+                    userStakedNFTCount[msg.sender]
+                )
+            );
+            userHasRank[msg.sender] = true;
+        } else {
+            uint256 rankId = userRankIds[msg.sender];
+            ranks[rankId].totalBattlePower = _battlePowerBalances[msg.sender];
+            ranks[rankId].amount = userStakedNFTCount[msg.sender];
+        }
+    }
+
+    function calUserShare(address userAddress) internal {
+        _totalSupply
+
+    }
+
+    //******************** sorting **********************//
+    function sort(Rank[] memory data) private pure returns (Rank[] memory) {
+        quickSort(data, int256(0), int256(data.length - 1));
+        return data;
+    }
+
+    function quickSort(
+        Rank[] memory arr,
+        int256 left,
+        int256 right
+    ) private pure {
+        int256 i = left;
+        int256 j = right;
+        if (i == j) return;
+        Rank memory pivot = arr[uint256(left + (right - left) / 2)];
+        while (i <= j) {
+            while (arr[uint256(i)].totalBattlePower > pivot.totalBattlePower)
+                i++;
+            while (pivot.totalBattlePower > arr[uint256(j)].totalBattlePower)
+                j--;
+            if (i <= j) {
+                (arr[uint256(i)], arr[uint256(j)]) = (
+                    arr[uint256(j)],
+                    arr[uint256(i)]
+                );
+                i++;
+                j--;
+            }
+        }
+        if (left < j) quickSort(arr, left, j);
+        if (i < right) quickSort(arr, i, right);
+    }
+
+    //*************** transfer *********************//
+
+    function transferFee(address payable _to, uint256 _amount)
+        public
+        onlyOwner
+    {
+        (bool success, ) = _to.call{value: _amount}("");
+        require(success, "Failed to send Ether");
+    }
+
+    function transferReward(
+        address _contractAddress,
+        address _to,
+        uint256 _amount
+    ) public onlyOwner {
+        IERC20 _token = IERC20(_contractAddress);
+        _token.transfer(_to, _amount);
     }
 }
